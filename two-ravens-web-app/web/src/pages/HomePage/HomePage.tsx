be76ba0b78e71863a78b662984e92bc82ad7a175
Jedidiah <hello@jedidiah.dev>
2022-08-03 23:38:09 +0200

    Add temporary HomePage

---
+++ two-ravens-web-app/web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,15 @@
+import { MetaTags } from '@redwoodjs/web';
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Two Ravens" description="Memory and Thought" />
+
+      <h1>JGI CodeForGood Hackathon</h1>
+      <h2>Two Ravens</h2>
+      <p>Soon TM</p>
+    </>
+  );
+};
+
+export default HomePage;
