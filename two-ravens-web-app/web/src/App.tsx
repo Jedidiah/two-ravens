fc23ef6a78d0cec4990212d37bda32c98dfc4465
Jedidiah <hello@jedidiah.dev>
2022-08-04 02:30:05 +0200

    Move Spectrum Provider out of routes

--- two-ravens-web-app/web/src/App.tsx
+++ two-ravens-web-app/web/src/App.tsx
@@ -1,24 +1,27 @@
-import { AuthProvider } from '@redwoodjs/auth'
+import { defaultTheme, Provider } from '@adobe/react-spectrum';
 
-import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
-import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
+import { AuthProvider } from '@redwoodjs/auth';
+import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web';
+import { RedwoodApolloProvider } from '@redwoodjs/web/apollo';
 
-import FatalErrorPage from 'src/pages/FatalErrorPage'
-import Routes from 'src/Routes'
+import FatalErrorPage from 'src/pages/FatalErrorPage';
+import Routes from 'src/Routes';
 
-import './scaffold.css'
-import './index.css'
+import './scaffold.css';
+import './index.css';
 
 const App = () => (
-  <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <AuthProvider type="dbAuth">
-        <RedwoodApolloProvider>
-          <Routes />
-        </RedwoodApolloProvider>
-      </AuthProvider>
-    </RedwoodProvider>
-  </FatalErrorBoundary>
-)
+  <Provider theme={defaultTheme}>
+    <FatalErrorBoundary page={FatalErrorPage}>
+      <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
+        <AuthProvider type="dbAuth">
+          <RedwoodApolloProvider>
+            <Routes />
+          </RedwoodApolloProvider>
+        </AuthProvider>
+      </RedwoodProvider>
+    </FatalErrorBoundary>
+  </Provider>
+);
 
-export default App
+export default App;
